0744b6fc27c556d2c4982e0397b43e6b3c7560e9
janikF1 <jolograggy45@gmail.com>
2023-01-24 12:18:33 +0100

    Grobentwurf admin technisch

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
+      //home: const AdminTechnisch(title: 'Startseite'),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
