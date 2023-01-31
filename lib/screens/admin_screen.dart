af04d8fad4cf331be1a67b48bdbc38621534cf4c
oriventi <adrianus3010@gmail.com>
2023-01-31 16:04:57 +0100

    SEP-64 Fixed deleting custom secretaries error

--- lib/screens/admin_screen.dart
+++ lib/screens/admin_screen.dart
@@ -19,15 +19,17 @@ class _TechnicalAdministrator extends State<TechnicalAdministratorScreen> {
   ///Map to save index of widget and secretariats name
 
   ///generates initial List with Secretariats while no database connection exists
-  late List<Widget> secretariats = List<Widget>.generate(
-      10,
-      (index) => AdminListTile(
-            key: ValueKey(index),
-            index: index + 1,
-            callAlert: callDeleteAlert,
-            name: 'Sekretariat ${index + 1}',
-            addNewNameToMap: addNewNameToMap,
-          ));
+  List<String> secretariatsNames =
+      List.generate(10, (index) => 'Sekretariat ${index + 1}');
+  // late List<Widget> secretariats = List<Widget>.generate(
+  //     10,
+  //     (index) => AdminListTile(
+  //           key: ValueKey(index),
+  //           index: index + 1,
+  //           callAlert: callDeleteAlert,
+  //           name: 'Sekretariat ${index + 1}',
+  //           addNewNameToMap: addNewNameToMap,
+  //         ));
 
   ///calls AlertDialog if delete button pressed
   void callDeleteAlert(String content, int index) {
@@ -46,25 +48,28 @@ class _TechnicalAdministrator extends State<TechnicalAdministratorScreen> {
 
   ///removes Secretariat from List
   void removeSecretariat(int index) {
+    debugPrint('$index , max: ${secretariatsNames.length}');
     setState(() {
-      final int key = index - 1;
-      final keyToRemove = ValueKey(key);
-      secretariats.removeWhere((item) => item.key == keyToRemove);
+      secretariatsNames.removeAt(index);
+      // final int key = index - 1;
+      // final keyToRemove = ValueKey(key);
+      // secretariats.removeWhere((item) => item.key == keyToRemove);
     });
   }
 
   /// add New Seccretariat to List
-  void addSecretariat() {
+  void addSecretariat(String name) {
     setState(() {
-      int newIndex = secretariats.length;
-      AdminListTile newSec = AdminListTile(
-          key: Key(newIndex.toString()),
-          index: key + 1,
-          callAlert: callDeleteAlert,
-          name: 'Sekretariat ${key + 1}',
-          addNewNameToMap: addNewNameToMap);
-      secretariats.add(newSec);
-      key++;
+      secretariatsNames.add(name);
+      // int newIndex = secretariats.length;
+      // AdminListTile newSec = AdminListTile(
+      //     key: Key(newIndex.toString()),
+      //     index: key + 1,
+      //     callAlert: callDeleteAlert,
+      //     name: 'Sekretariat ${key + 1}',
+      //     addNewNameToMap: addNewNameToMap);
+      // secretariats.add(newSec);
+      // key++;
     });
   }
 
@@ -88,28 +93,40 @@ class _TechnicalAdministrator extends State<TechnicalAdministratorScreen> {
           ],
         ),
         body: Padding(
-            padding: const EdgeInsets.only(top: 10.0, left: 10.0, right: 10.0),
-            child: SingleChildScrollView(
-                child: Column(
-              children: [
-                const Text(
-                  'Sekretariate',
-                  style: TextStyle(
-                    fontSize: 25,
-                  ),
+          padding: const EdgeInsets.only(top: 10.0, left: 10.0, right: 10.0),
+          child: Column(
+            children: [
+              const Text(
+                'Sekretariate',
+                style: TextStyle(
+                  fontSize: 25,
                 ),
-                const SizedBox(
-                  height: 15,
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              const Divider(
+                thickness: 5,
+              ),
+              Expanded(
+                child: ListView.builder(
+                  itemBuilder: (context, index) => AdminListTile(
+                      key: UniqueKey(),
+                      index: index,
+                      callAlert: callDeleteAlert,
+                      name: secretariatsNames[index],
+                      addNewNameToMap: addNewNameToMap),
+                  itemCount: secretariatsNames.length,
                 ),
-                const Divider(
-                  thickness: 5,
-                ),
-                for (int i = 0; i < secretariats.length; i++) (secretariats[i])
-              ],
-            ))),
+              ),
+              // for (int i = 0; i < secretariats.length; i++) (secretariats[i])
+            ],
+          ),
+        ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
-            addSecretariat();
+            addSecretariat(
+                'Sekretariat ${(int.tryParse(secretariatsNames.last.split(' ')[1]) ?? secretariatsNames.length) + 1}');
           },
           tooltip: 'Sekretariat hinzufügen',
           child: const Icon(Icons.add),
