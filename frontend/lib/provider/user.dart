391c5cbc60cbfbbdea5e6d74a7cd0019637e6be8
janikF1 <jolograggy45@gmail.com>
2023-03-31 15:11:11 +0200

    change password

--- frontend/lib/provider/user.dart
+++ frontend/lib/provider/user.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:frontend/models/user_role.dart';
+import '../utils/authenticated_request.dart';
 import '../utils/constants.dart' as constants;
 import '../utils/db_constants.dart' as db;
 
@@ -18,14 +21,27 @@ class User with ChangeNotifier {
     required this.email,
   });
 
-  void changePassword(String oldPwd, String newPwdOne, String newPwdTwo) {
-    String newPwd = '';
-
-    ///TODO check if old pwd is correct
-    if (newPwdOne == newPwdTwo) {
-      newPwd = newPwdOne;
+  void changePassword(String oldPwd, String newPwdOne, String newPwdTwo) async {
+    if (oldPwd == newPwdOne) {
+      throw Exception('Old and new password are the same');
     }
-
-    ///TODO save newpwd in database
+    if (newPwdOne != newPwdTwo) {
+      throw Exception('New passwords do not match');
+    }
+    if (newPwdOne == newPwdTwo && oldPwd != newPwdOne) {
+      var response = await AuthHttp.get(
+          "http://homenetwork-test.ddns.net:5160/api/user/$id");
+      if (response.statusCode == 200) {
+        var user = jsonDecode(response.body);
+        if (user["password"] != oldPwd) {
+          throw Exception('Old password is incorrect');
+        } else {
+          user["password"] = newPwdOne;
+          await AuthHttp.put(
+              "http://homenetwork-test.ddns.net:5160/api/user/$id",
+              body: jsonEncode(user));
+        }
+      }
+    }
   }
 }
